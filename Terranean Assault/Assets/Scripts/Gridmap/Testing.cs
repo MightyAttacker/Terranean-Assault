cf61c2f1beeb51fb122370667d6e23680460a46f
Lachlan Klenk <klen0010@flinders.edu.au>
2025-09-08 10:36:06 +0930

    Grid System

--- Terranean Assault/Assets/Scripts/Gridmap/Testing.cs
+++ Terranean Assault/Assets/Scripts/Gridmap/Testing.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
+using CodeMonkey.Utils;
 
 public class Testing : MonoBehaviour
 {
+    private Grid grid;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Grid grid = new Grid(4, 2, 1f);
+        grid = new Grid(44, 30, 1f, new Vector3(-10, -20));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Testing for left click
+        if (Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(UtilsClass.GetMouseWorldPosition(), 56);
+        }
+
+        // Testing for right click
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(grid.GetValue(UtilsClass.GetMouseWorldPosition()));
+        }
     }
 }
