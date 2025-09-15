dc71f74a364ef3cdea1eb74da42b0aef719d259b
ngir0003 <ngir0003@flinders.edu.au>
2025-09-15 13:03:16 +0930

    Accpet/Cancel/Option Button
    Option Button (main menu) loads the option menu.
    Accept/Cancel buttons load the main menu

--- Terranean Assault/Assets/Scripts/Menu/Options/OptionsMenu.cs
+++ Terranean Assault/Assets/Scripts/Menu/Options/OptionsMenu.cs
@@ -1,6 +1,14 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class OptionsMenu
+public class OptionsMenu : MonoBehaviour
 {
-    
+    public void Accept()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+    public void Cancel()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
 }
