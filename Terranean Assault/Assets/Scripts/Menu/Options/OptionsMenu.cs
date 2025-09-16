51ee767aea0bd6c475518d9dd7792df118adea72
ngir0003 <ngir0003@flinders.edu.au>
2025-09-16 15:57:23 +0930

    settings UI
    Improved on setting UI

--- Terranean Assault/Assets/Scripts/Menu/Options/OptionsMenu.cs
+++ Terranean Assault/Assets/Scripts/Menu/Options/OptionsMenu.cs
@@ -1,14 +1,55 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
+    [SerializeField] private Scrollbar brightnessScrollbar;
+    [SerializeField] private Image screenOverlay;
+
+    private void Start()
+    {
+
+        if (brightnessScrollbar != null && screenOverlay != null)
+        {
+            brightnessScrollbar.onValueChanged.AddListener(UpdateBrightness);
+
+            brightnessScrollbar.value = PlayerPrefs.GetFloat("Brightness", 1f);
+            UpdateBrightness(brightnessScrollbar.value);
+        }
+    }
+
+
+    private void UpdateBrightness(float value)
+    {
+        if (screenOverlay != null)
+        {
+
+            float alpha = Mathf.Lerp(0.8f, 0f, value);
+            Color overlayColor = screenOverlay.color;
+            overlayColor.a = alpha;
+            screenOverlay.color = overlayColor;
+        }
+    }
+
     public void Accept()
     {
+        if (brightnessScrollbar != null)
+            PlayerPrefs.SetFloat("Brightness", brightnessScrollbar.value);
+        
         SceneManager.LoadSceneAsync(0);
     }
+
     public void Cancel()
     {
+
         SceneManager.LoadSceneAsync(0);
+    }
+
+    private void OnDestroy()
+    {
+
+        if (brightnessScrollbar != null)
+            brightnessScrollbar.onValueChanged.RemoveListener(UpdateBrightness);
     }
 }
